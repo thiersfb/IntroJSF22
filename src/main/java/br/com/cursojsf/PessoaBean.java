6a324ca2164b64ccf6afe737e127e5c7edb98aee
thiersfb <thiers.fb@gmail.com>
2022-11-29 10:29:38 -0300

    Ëxemplo RequestScoped

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,17 +1,30 @@
 package br.com.cursojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean(name = "pessoaBean")
+@RequestScoped
 public class PessoaBean {
 	
 	private String nome;
-	private String sobrenome;
-	private String nomeCompleto;
+
+	private List<String> nomes = new ArrayList<String>();
 	
-	public String mostrarNome() {
-		nomeCompleto = nome + " " + sobrenome;
+	public String addNome() {
+		nomes.add(nome);
 		return " ";
+	}
+	
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+	
+	public List<String> getNomes() {
+		return nomes;
 	}
 	
 	public String getNome() {
@@ -20,24 +33,5 @@ public class PessoaBean {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
-	
-	
-	
+	}	
 }
