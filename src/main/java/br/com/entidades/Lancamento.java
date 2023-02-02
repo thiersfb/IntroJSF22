acbaacdeddca99ca6e06b0f39ac9e11c139843b0
thiersfb <thiers.fb@gmail.com>
2023-02-02 20:53:09 -0300

    Menu básico e modelo de lançamento

---
+++ src/main/java/br/com/entidades/Lancamento.java
@@ -0,0 +1,111 @@
+package br.com.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity(name = "TBLancamento")
+@Table(name = "TBLancamento")
+public class Lancamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String numeroNF;
+	private String serieNF;
+	private String empresaOrigem;
+	private String empresaDestino;
+	
+	@ManyToOne(optional = false)
+	//@javax.persistence.ForeignKey(name="usuario_fk")
+	@JoinColumns(foreignKey = @ForeignKey(name="usuario_fk"), value = { @JoinColumn(referencedColumnName = "id") })
+	private Pessoa usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumeroNF() {
+		return numeroNF;
+	}
+
+	public void setNumeroNF(String numeroNF) {
+		this.numeroNF = numeroNF;
+	}
+
+	public String getSerieNF() {
+		return serieNF;
+	}
+
+	public void setSerieNF(String serieNF) {
+		this.serieNF = serieNF;
+	}
+
+	public String getEmpresaOrigem() {
+		return empresaOrigem;
+	}
+
+	public void setEmpresaOrigem(String empresaOrigem) {
+		this.empresaOrigem = empresaOrigem;
+	}
+
+	public String getEmpresaDestino() {
+		return empresaDestino;
+	}
+
+	public void setEmpresaDestino(String empresaDestino) {
+		this.empresaDestino = empresaDestino;
+	}
+
+	public Pessoa getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Pessoa usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lancamento other = (Lancamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
