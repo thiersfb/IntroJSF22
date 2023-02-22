b4930cfdb1bf989d9436ebf1f713ae3415ab696e
thiersfb <thiers.fb@gmail.com>
2023-02-22 14:05:47 -0300

    Mostrando Imagem em Tela

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.jar.Attributes.Name;
 
 import javax.faces.bean.ViewScoped;
@@ -313,5 +314,11 @@ public class PessoaBean {
 		}
 		
 		return buffer;
+	}
+	
+	public void download() {
+ 		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String fileDownloadId = params.get("fileDownloadId");
+		System.out.println(fileDownloadId);
 	}
 }
