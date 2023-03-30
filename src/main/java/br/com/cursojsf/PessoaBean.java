a2df474798fdccbeeb405d4c4f91bda4588f43b7
thiersfb <thiers.fb@gmail.com>
2023-03-30 18:28:20 -0300

    Configurando o CDI - Context Dependency Inject

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -56,6 +56,8 @@ public class PessoaBean {
 	
 	private IDaoPessoa iDaoPessoa = new IDaoPessoaImplement();
 	
+	//private IDaoPessoa iDaoPessoa;
+	
 	private List<SelectItem> estados;
 	private List<SelectItem> cidades;
 	
